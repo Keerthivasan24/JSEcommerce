a5c0d1b3c94e47f01a1ef1561d123f49c8c0a9d1
Keerthivasan24 <keerthivasan5801@gmail.com>
2024-07-15 12:51:54 +0530

    15/07/24

---
+++ JS/sample.js
@@ -0,0 +1,37 @@
+let arrofbj = [
+    {
+       id:'01',
+       img:'/photos/img1.png',
+       price:234,
+       description:'sample description of cracker image',
+       review:'so what good'
+    },
+    {
+        id:'02',
+        image:'/photos/img2.png',
+        price:234,
+        description:'sample description of cracker image',
+        review:'so what good'
+     },
+     {
+        id:'03',
+        image:'/photos/img3.png',
+        price:234,
+        description:'sample description of cracker image',
+        review:'so what good'
+     },
+     {
+        id:'04',
+        image:'/photos/img4.png',
+        price:234,
+        description:'sample description of cracker image',
+        review:'so what good'
+     },
+     {
+        id:'05',
+        image:'/photos/img5.png',
+        price:234,
+        description:'sample description of cracker image',
+        review:'so what good'
+     },
+]
